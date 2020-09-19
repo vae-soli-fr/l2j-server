a577207bf8d9cc1bf1e6a7c79d53d3e9373c610d
Melua <melua@melua.fr>
2020-09-19 20:42:06 +0200

    Add run script

---
+++ dist/game/game.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+NAME=`basename "$0" ".sh"`
+JAVA=`which java`
+MIN=1g
+MAX=8g
+
+# exit codes of GameServer:
+#  0 normal shutdown
+#  2 reboot attempt
+
+while :; do
+	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
+	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
+	$JAVA -server \
+	-Xms$MIN -Xmx$MAX \
+	-Djava.util.logging.manager=com.l2jserver.util.L2LogManager \
+	-Dpython.cachedir=../cachedir \
+	-Djava.net.preferIPv4Stack=true \
+	-Djava.net.preferIPv4Addresses=true \
+	-jar l2jserver.jar > log/stdout.log 2>&1 &
+	GS_PID=$!
+	echo $GS_PID > $NAME.pid
+	wait $GS_PID
+	[ $? -ne 2 ] && break
+	sleep 20
+done
