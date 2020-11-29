bc4733a8e7605cb6f0faed0ef19f5959a2708166
Melua <melua@melua.fr>
2020-11-29 19:55:41 +0100

    Add run script

---
+++ dist/login/login.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+NAME=login
+JAVA=`which java`
+USER=`whoami`
+MIN=64m
+MAX=128m
+
+if [ $USER != 'l2jserver' ]; then
+	echo "This script must be run as l2jserver user not $USER."
+	exit;
+fi
+
+# exit codes of LoginServer:
+#  0 normal shutdown
+#  2 reboot attempt
+
+while :; do
+	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
+	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
+	$JAVA -server \
+	-Xms$MIN -Xmx$MAX \
+	-XX:+UseG1GC \
+	-Djava.net.preferIPv4Stack=true \
+	-Djava.net.preferIPv4Addresses=true \
+	-jar l2jlogin.jar > log/stdout.log 2>&1 &
+	LS_PID=$!
+	echo $LS_PID > $NAME.pid
+	wait $LS_PID
+	[ $? -ne 2 ] && break
+	sleep 20
+done
