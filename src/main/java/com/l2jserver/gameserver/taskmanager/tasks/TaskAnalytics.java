a67ec5dcf027eda0db67109c6a65d715a21f9925
Melua <melua@melua.fr>
2021-01-24 13:17:50 +0100

    Update analytics with characters names

--- src/main/java/com/l2jserver/gameserver/taskmanager/tasks/TaskAnalytics.java
+++ src/main/java/com/l2jserver/gameserver/taskmanager/tasks/TaskAnalytics.java
@@ -1,9 +1,13 @@
 package com.l2jserver.gameserver.taskmanager.tasks;
 
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -15,6 +19,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
+import com.google.common.base.Charsets;
 import com.l2jserver.Config;
 import com.l2jserver.commons.database.pool.impl.ConnectionFactory;
 import com.l2jserver.gameserver.model.L2World;
@@ -51,14 +56,40 @@ public class TaskAnalytics extends Task
 
 	private void saveAnalytics()
 	{
+		int onlinePlayers = 0;
+		int blueEvas = getItemTotal(BLUE_EVA);
+
+		List<String> offlineChars = new LinkedList<>();
+		List<String> onlineGMs = new LinkedList<>();
+		List<String> onlineChars = new LinkedList<>();
+
+		for (L2PcInstance player : L2World.getInstance().getPlayersSortedBy(Comparator.comparingLong(L2PcInstance::getUptime))) {
+			if (player.isInOfflineMode()) {
+				offlineChars.add(player.getName());
+				continue;
+			}
+
+			onlinePlayers++;
+
+			if (player.isGM()) {
+				onlineGMs.add(player.getName());
+				continue;
+			}
+
+			onlineChars.add(player.getName());
+		}
+
 		try (CloseableHttpClient httpclient = HttpClientBuilder.create().disableCookieManagement().build()) {
 
 			HttpPost httpPost = new HttpPost("https://api.vae-soli.fr/stats.php");
 			List<NameValuePair> nvps = new ArrayList<>();
-			nvps.add(new BasicNameValuePair("onlinePlayers", String.valueOf(getOnlinePlayers())));
-			nvps.add(new BasicNameValuePair("blueEvas", String.valueOf(getItemTotal(BLUE_EVA))));
+			nvps.add(new BasicNameValuePair("onlinePlayers", String.valueOf(onlinePlayers)));
+			nvps.add(new BasicNameValuePair("blueEvas", String.valueOf(blueEvas)));
+			nvps.add(new BasicNameValuePair("onlineChars", String.join(";", onlineChars)));
+			nvps.add(new BasicNameValuePair("offlineChars", String.join(";", offlineChars)));
+			nvps.add(new BasicNameValuePair("onlineGMs", String.join(";", onlineGMs)));
 			nvps.add(new BasicNameValuePair("secret", Config.API_SECRET));
-			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps, Charsets.UTF_8));
 
 			try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
 
@@ -71,17 +102,6 @@ public class TaskAnalytics extends Task
 		} catch (Exception e) {
 			_log.log(Level.WARNING, "Exception while calling API stats.", e);
 		}
-	}
-
-	private int getOnlinePlayers() {
-		int count = 0;
-		for (L2PcInstance player : L2World.getInstance().getPlayers()) {
-			if (player.isInOfflineMode()) {
-				continue;
-			}
-			count++;
-		}
-		return count;
 	}
 
 	private int getItemTotal(int itemId) {
