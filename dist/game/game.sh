0665c63712507ce4021431cc669ffca8d3cb01cf
Melua <melua@melua.fr>
2024-09-22 17:27:14 +0200

    Update script

--- dist/game/game.sh
+++ dist/game/game.sh
@@ -1,41 +1,24 @@
-#!/bin/bash
-NAME=`basename "$0" ".sh"`
-JAVA=`which java`
-USER=`whoami`
-MIN=1g
-MAX=3g
-
-if [ $USER != 'l2jserver' ]; then
-	echo "This script must be run as l2jserver user not $USER."
-	exit;
-fi
+#!/bin/sh
+. setEnv.sh
 
 # exit codes of GameServer:
 #  0 normal shutdown
 #  2 reboot attempt
-
 while :; do
-	source setEnv.sh
 	[ -f l2jserver.jar.new ] && mv l2jserver.jar.new l2jserver.jar
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
-	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
 	[ -f log/item.log ] && mv log/item.log "log/`date +%Y-%m-%d_%H-%M-%S`_item.log"
 	[ -f log/chat.log ] && mv log/chat.log "log/`date +%Y-%m-%d_%H-%M-%S`_chat.log"
-	$JAVA -server $JAVA_OPTS \
-	-Xms$MIN -Xmx$MAX \
+	/usr/bin/java $JAVA_OPTS \
+	-Xms$JAVA_MIN_MEMORY -Xmx$JAVA_MAX_MEMORY \
 	-XX:+UseG1GC \
 	-Djava.util.logging.manager=com.l2jserver.util.L2LogManager \
 	-Dpython.cachedir=../cachedir \
-	-Dhttps.proxyHost=proxy \
-	-Dhttps.proxyPort=3128 \
+	-Dhttps.proxyHost=$JAVA_PROXY_HOST \
+	-Dhttps.proxyPort=$JAVA_PROXY_PORT \
 	-Djava.net.preferIPv4Stack=true \
-	-Djava.net.preferIPv4Addresses=true \
-	-Dcom.sun.management.config.file=jmx.cfg \
-	-Djava.rmi.server.hostname=gameserver.lan \
-	-jar l2jserver.jar > log/stdout.log 2>&1 &
-	GS_PID=$!
-	echo $GS_PID > $NAME.pid
-	wait $GS_PID
+	-jar l2jserver.jar &
+	wait $!
 	[ $? -ne 2 ] && break
 	sleep 20
 done
